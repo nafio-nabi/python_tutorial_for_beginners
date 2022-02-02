27ee5988ef13baa44cb278de74ff9def11d6dc6f
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-02-02 16:41:03 +0600

    Lists.

--- 2_data_types/lists.py
+++ 2_data_types/lists.py
@@ -2,3 +2,20 @@ data_types = ["String", 100, 99.99, True, False]
 
 for data in data_types:
     print(f"{type(data)}: {data}")
+
+print()
+
+months = ["January", "February", "March"]
+print(months[0])
+print(months[1])
+
+print()
+
+months.append("April")
+for month in months:
+    print(month)
+
+print()
+
+# error: IndexError
+# print(months[4])
