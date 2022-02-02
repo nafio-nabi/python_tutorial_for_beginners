41962aee2c5f4128b16149bf6f81b7d01ac6442b
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-02-02 09:56:52 +0600

    Variables.

---
+++ 3_variables/variables.py
@@ -0,0 +1,15 @@
+# seconds
+# calculation_to_units = 24 * 60 * 60
+# name_of_unit = "seconds"
+
+# minutes
+# calculation_to_units = 24 * 60
+# name_of_unit = "minutes"
+
+# hours
+calculation_to_units = 24
+name_of_unit = "hours"
+
+print(f"20 days are {20 * calculation_to_units} {name_of_unit}.")
+print(f"35 days are {35 * calculation_to_units} {name_of_unit}.")
+print(f"50 days are {50 * calculation_to_units} {name_of_unit}.")
