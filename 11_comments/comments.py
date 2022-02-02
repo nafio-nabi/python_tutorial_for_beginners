6e94b934ed2d48694b4875d56375f16603ecd9b0
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-02-02 16:48:37 +0600

    Comments

---
+++ 11_comments/comments.py
@@ -0,0 +1,26 @@
+calculation_to_units = 24
+name_of_unit = "hours"
+
+def days_to_units(num_of_days):
+    """Convert days to units"""
+    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}."
+
+def validate_and_execute():
+    try:
+        user_input_number = int(num_of_days_element)
+        # we want to do conversions only for positive integers.
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("You entered zero, please enter a valid positive number.")
+        else:
+            print("You entered a negative number, please enter a valid positive number.")
+    except ValueError:
+        print("Your input is not a valid number. Please enter a positive number.")
+
+user_input = ""
+while user_input != "exit":
+    user_input = input("Enter number of days as a comma seperated list: ")
+    for num_of_days_element in user_input.split(","):
+        validate_and_execute()
