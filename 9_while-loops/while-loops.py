7d01fcdb6bd7fa8fb5fbfc1b2e5f8efff854b07b
Nafio Nabi <nafionabi.work.acc@gmail.com>
2022-02-02 16:01:03 +0600

    While loops.

---
+++ 9_while-loops/while-loops.py
@@ -0,0 +1,23 @@
+calculation_to_units = 24
+name_of_unit = "hours"
+
+def days_to_units(num_of_days):
+    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}."
+
+def validate_and_execute():
+    try:
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("You entered zero, please enter a valid positive number.")
+        else:
+            print("You entered a negative number, please enter a valid positive number.")
+    except ValueError:
+        print("Your input is not a valid number. Please enter a positive number.")
+
+user_input = ""
+while user_input != "exit":
+    user_input = input("Enter number of days: ")
+    validate_and_execute()
